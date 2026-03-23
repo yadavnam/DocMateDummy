3f4a2a08c1b868eb00a8fdc3fc11dc4005d862f1
NAMAN PANKAJ YADAV <04yadavnaman@gmail.com>
2026-03-23 23:26:28 +0530

    Initial Commit

---
+++ DocMateDummy/DocMateDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocMateDummyApp.swift
+//  DocMateDummy
+//
+//  Created by Naman Yadav on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DocMateDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
